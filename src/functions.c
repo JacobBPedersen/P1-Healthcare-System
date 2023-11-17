9d34199199f559bf2f69919b52069e71845039c5
GizmoFellow <143991769+GizmoFellow@users.noreply.github.com>
2023-11-17 10:32:39 +0100

    spooky scary skeleton

--- src/functions.c
+++ src/functions.c
@@ -2,3 +2,29 @@
 void print_func(){
     printf("Hello World!");
 }
+
+
+
+void user_cred() {
+    //Takes input as to who is using the program, and what it is supposed to do at the moment
+}
+
+void search_referral (){
+    //searches the register through some ID
+}
+
+void create_referral() {
+    //checks if the patient is already registered.
+    //if not, a patient is created from scratch with create_patient
+    //otherwise only asks for variable data in update_patient
+    //saves patient data in the database
+}
+
+
+
+void review_referral() {
+    //access all referrals - sort either through prioritization or chronologically
+    //option to see current time schedule
+    //access specific referrals for review
+    //create time in an available time slot
+}
