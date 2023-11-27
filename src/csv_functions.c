f7dc94ac4d7405e73f4c2d14d0c22d30315670b1
William <xx31ta@student.aau.dk>
2023-11-27 12:14:09 +0100

    Jeg skubber den lige ind :3

--- src/csv_functions.c
+++ src/csv_functions.c
@@ -8,8 +8,6 @@ char *search_cpr(char *cpr, FILE *file)
     char *return_buffer = malloc(150 * sizeof(char));
     char *token;
 
-    int found = 0;
-
     while (fgets(buffer, sizeof(char)*150, file) != NULL)
     {
         strcpy(return_buffer, buffer);
@@ -17,14 +15,11 @@ char *search_cpr(char *cpr, FILE *file)
 
         if (strcmp(token, cpr) == 0)
         {
-            found = 1;
-        }
 
-        if (found)
-        {
             fclose(file);
             return return_buffer;
         }
+
     }
     return "value not found";
 }
